ed1a5b41c4f71405b81242f50b58dbbad02a2412
SystemEdbar <systemedbar@gmail.com>
2023-10-28 00:39:00 -0600

    change on contacv2

--- resources/views/contac.blade.php
+++ resources/views/contac.blade.php
@@ -48,7 +48,7 @@
         </div>
         <div class="mt-16">
             <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-8">
-                <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white from-gray-700/50 via-transparent rounded-lg shadow-2xl shadow-gray-500/20 flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                <a class="scale-100 p-6 bg-white from-gray-700/50 via-transparent rounded-lg shadow-2xl shadow-gray-500/20 flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                     <div>
                         <h2 class="mt-6 text-xl font-semibold text-gray-900">Misión:</h2>
                         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
